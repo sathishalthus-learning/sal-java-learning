68e37c984ae2d0ee2228b32b3f307b92d230e3cd
sathishalthus-learning <sathishalthus.learning@gmail.com>
2025-08-25 14:53:44 +0530

    First Revision

--- src/test/java/learning/baeldung/io/File02_Read.java
+++ src/test/java/learning/baeldung/io/File02_Read.java
@@ -1,5 +1,24 @@
 package learning.baeldung.io;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+// import java.io.InputStream;
+// import java.io.InputStreamReader;
+
 public class File02_Read {
+
+    public static void main(String[] args) throws IOException {
     
+        // read from InputStream
+    try (BufferedReader br = new BufferedReader
+                                    (new FileReader("src\\test\\java\\resources\\readFile.txt")))
+    {
+        System.out.println(br.readLine());
+    }   
+
+
+    // path class
+    
+}
 }
---
+++ src/test/java/learning/dev/io/File02_ManipulatingFiles.java
@@ -0,0 +1,33 @@
+package learning.dev.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class File02_ManipulatingFiles {
+
+    public static void main(String[] args) throws IOException {
+        
+        Path path = Paths.get("src\\test\\java\\resources");
+
+        // checking for a file/dir
+        boolean exists = Files.exists(path);
+        System.out.println("path exists : " + exists);
+
+        // same file ?
+        // copy file
+        
+        // delete file
+        try {
+            Files.delete(path);
+            System.out.println("file deleted");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        // no exception thrown
+        Files.deleteIfExists(path);
+        //
+    }
+    
+}
